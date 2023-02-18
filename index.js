94178ec98b5ce6d36d795f4379f3c565dc0ea5de
tarikul-islam <tariikul12@gmail.com>
2023-02-18 12:41:19 +0600

    All Card design is done

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 let serial = 0;
 
+const allCard = document.querySelectorAll('.bg-color');
+for (const card of allCard) {
+  card.addEventListener('mouseenter', () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    card.style.backgroundColor = "#" + randomColor;
+    card.addEventListener('mouseleave', () => {
+      card.style.backgroundColor = '';
+    });
+  });
+}
+function checkTrue( idCheck){
+    const b = document.getElementById(idCheck);
+    b.disabled = true;
+}
+function checkFalse( idCheck){
+    const b = document.getElementById(idCheck);
+    b.disabled = false;
+}
 
 function inputFieldId (inputId){
     const depositFieldInput = document.getElementById(inputId);
@@ -15,39 +33,49 @@ function innerTextId (innerTextId){
     depositFieldInput.innerText = '';
     return depositField; 
 }
+function setValue (id,newValue){
+    const widthTriangle =document.getElementById(id);
+    widthTriangle.value = newValue;  
+    console.log(newValue)
+}
+function setText (id,newValue){
+    const widthTriangle =document.getElementById(id);
+    widthTriangle.innerText = newValue;  
+    console.log(newValue)
+}
 document.getElementById('a').addEventListener('click',function(){
-    const b = document.getElementById('width-triangle');
-    b.disabled = false;
-    const h = document.getElementById('height-triangle');
-    h.disabled = false;
+    checkFalse( 'width-triangle');
+    checkFalse( 'height-triangle');
     const bInnerText = innerTextId('b')
     const hInnerText = innerTextId('h')
-    const widthTriangle =document.getElementById('width-triangle');
-    widthTriangle.value = bInnerText;  
-    const heightTriangle = document.getElementById('height-triangle');
-    heightTriangle.value = hInnerText;
+    if(isNaN(bInnerText) === true || isNaN(hInnerText) === true){
+        return alert('ah');
+    }
+    setValue ('width-triangle',bInnerText);
+    setValue ('height-triangle',hInnerText);
 })
-
 document.getElementById('check').addEventListener('click',function(){
-    const b = document.getElementById('width-triangle');
-    b.disabled = true;
-    const h = document.getElementById('height-triangle');
-    h.disabled = true;
     const widthTriangle = inputFieldId('width-triangle')
     const heightTriangle = inputFieldId('height-triangle')
-    const bInnerText =document.getElementById('b');
-    bInnerText.innerText = widthTriangle;  
-    const hInnerText = document.getElementById('h');
-    hInnerText.innerText = heightTriangle;
-
+    if(isNaN(widthTriangle) === true || isNaN(heightTriangle) === true || widthTriangle<0 || heightTriangle<0 ){
+        return alert('ah');
+    }
+    checkTrue( 'width-triangle');
+    checkTrue( 'height-triangle');
+    setText ('b',widthTriangle);
+    setText ('h',heightTriangle);
+})
+document.getElementById('convert-to-meter').addEventListener('click',function(){
     
 })
-
 document.getElementById('btn-triangle').addEventListener('click',function(){
     const nameTriangle = document.getElementById('title-triangle').innerText;
     const widthTriangle = inputFieldId('width-triangle')
     const heightTriangle = inputFieldId('height-triangle')
     const triangleArea = 0.5 * widthTriangle * heightTriangle;
+    if(isNaN(triangleArea) === true || widthTriangle<0 || heightTriangle<0 ){
+        return alert('ah');
+    }
     serial += 1;
     displayData(nameTriangle, triangleArea)
 })
@@ -56,6 +84,56 @@ document.getElementById('btn-triangle').addEventListener('click',function(){
 
 
 
+// document.getElementById('check').addEventListener('click',function(){
+//     const widthTriangle = inputFieldId('width-triangle')
+//     const heightTriangle = inputFieldId('height-triangle')
+//     if(isNaN(widthTriangle) === true || isNaN(heightTriangle) === true || widthTriangle<0 || heightTriangle<0 ){
+//         return alert('ah');
+//     }
+//     const b = document.getElementById('width-triangle');
+//     b.disabled = true;
+//     const h = document.getElementById('height-triangle');
+//     h.disabled = true;
+//     setText ('width-triangle',bInnerText);
+//     setText ('height-triangle',hInnerText);
+//     const bInnerText =document.getElementById('b');
+//     bInnerText.innerText = widthTriangle;  
+//     const hInnerText = document.getElementById('h');
+//     hInnerText.innerText = heightTriangle;  
+// })
+
+// document.getElementById('a').addEventListener('click',function(){
+//     // const b = document.getElementById();
+//     // b.disabled = false;
+//      check( 'width-triangle');
+//      check( 'height-triangle');
+//     // const h = document.getElementById();
+//     // h.disabled = false;
+//     const bInnerText = innerTextId('b')
+//     const hInnerText = innerTextId('h')
+//     if(isNaN(bInnerText) === true || isNaN(hInnerText) === true){
+//         return alert('ah');
+//     }
+//     setValue ('width-triangle',bInnerText);
+//     setValue ('height-triangle',hInnerText);
+// })
+
+
+// document.getElementById('check').addEventListener('click',function(){
+//     const widthTriangle = inputFieldId('width-triangle')
+//     const heightTriangle = inputFieldId('height-triangle')
+//     if(isNaN(widthTriangle) === true || isNaN(heightTriangle) === true || widthTriangle<0 || heightTriangle<0 ){
+//         return alert('ah');
+//     }
+//     // const b = document.getElementById('width-triangle');
+//     // b.disabled = true;
+//     // const h = document.getElementById('height-triangle');
+//     // h.disabled = true;
+//     checkTrue( 'width-triangle');
+//     checkTrue( 'height-triangle');
+//     setText ('b',widthTriangle);
+//     setText ('h',heightTriangle);
+// })
 
 
 
